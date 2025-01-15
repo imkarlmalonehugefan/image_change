764b1693ac67e7ed69d87aa480416c04c2903860
malone <goldeneagle2011@naver.com>
2025-01-15 10:53:21 +0900

    FIX

---
+++ index.js
@@ -0,0 +1,32 @@
+setInterval(()=>{
+
+
+
+
+let imgs=document.querySelectorAll('img');
+imgs.forEach((a,i)=>{
+    a.src='https://arc-anglerfish-arc2-prod-sltrib.s3.amazonaws.com/public/QWO2VP6CSBGMLDBCD5HAN5MMCI.jpg'
+})
+
+
+}, 500)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
